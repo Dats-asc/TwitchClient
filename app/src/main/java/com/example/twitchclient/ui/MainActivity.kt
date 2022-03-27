a262e45bfdb44eb7344b10f47bc6adc880cdd3e6
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2022-03-27 19:23:30 +0300

    Fragment navigation

--- app/src/main/java/com/example/twitchclient/ui/MainActivity.kt
+++ app/src/main/java/com/example/twitchclient/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.twitchclient.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,7 +12,9 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.twitchclient.R
 import com.example.twitchclient.databinding.ActivityMainBinding
 import com.example.twitchclient.ui.auth.AuthorizationActivity
+import com.example.twitchclient.ui.followings.FollowingsFragment
 import com.example.twitchclient.ui.games.GamesFragment
+import com.example.twitchclient.ui.popular.PopularFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
@@ -25,21 +28,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        fragmentContainer = binding.navHostFragmentContainer
+        init()
+    }
 
+    private fun init(){
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation(){
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.navigation_games -> {
+                    itemGamesSelected()
                     true
                 }
                 R.id.navigation_popular -> {
+                    itemPopularSelected()
                     true
                 }
                 R.id.navigation_followings -> {
+                    itemFollowingsSelected()
                     true
                 }
                 else -> false
             }
+        }
+    }
+
+    private fun itemGamesSelected(){
+        replaceFragment(GamesFragment())
+    }
+
+    private fun itemPopularSelected(){
+        replaceFragment(PopularFragment())
+    }
+
+    private fun itemFollowingsSelected(){
+        replaceFragment(FollowingsFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().run {
+            replace(R.id.nav_host_fragment_container, fragment)
+            commit()
         }
     }
 }
--- app/src/main/java/com/example/twitchclient/ui/followings/FollowingsFragment.kt
+++ app/src/main/java/com/example/twitchclient/ui/followings/FollowingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.twitchclient.ui.followings
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.twitchclient.R
+import com.example.twitchclient.databinding.FollowingsFragmentBinding
+import com.example.twitchclient.ui.auth.AuthFragment
+import com.example.twitchclient.ui.auth.AuthorizationActivity
 
 class FollowingsFragment : Fragment() {
 
@@ -16,17 +20,30 @@ class FollowingsFragment : Fragment() {
 
     private lateinit var viewModel: FollowingsViewModel
 
+    private lateinit var binding: FollowingsFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.followings_fragment, container, false)
+    ): View? = FollowingsFragmentBinding.inflate(inflater, container, false)?.let {
+        binding = it
+        it.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignin.setOnClickListener {
+            parentFragmentManager.beginTransaction().run {
+                add(R.id.auth_fragment_container, AuthFragment())
+                commit()
+            }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FollowingsViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
